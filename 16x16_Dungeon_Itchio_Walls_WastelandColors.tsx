b045b65e36a933d68ab1184c2427b50751e1c877
Lily E.S <konngitas@gmail.com>
2018-11-06 13:17:35 +0100

    Changed tileset directory, changed tsx to use project tileset directory as source

--- 16x16_Dungeon_Itchio_Walls_WastelandColors.tsx
+++ 16x16_Dungeon_Itchio_Walls_WastelandColors.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="16x16_Dungeon_Itchio_Walls_WastelandColors" tilewidth="16" tileheight="16" tilecount="256" columns="16">
- <image source="../../Desktop/folder of things/itchio packs/16x16 Dungeon Tileset/0x72_16x16DungeonTileset_walls.v2.png" width="256" height="256"/>
+ <image source="tilesets/16x16 Dungeon Tileset/0x72_16x16DungeonTileset_walls.v2.png" width="256" height="256"/>
 </tileset>
